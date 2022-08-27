e46480646bee6c88f65f100c83523c2a447897d0
supershem24 <super.sj24@gmail.com>
2022-08-27 13:00:14 -0400

    Made Tiles
    Added TileData
    Added Tiles and the Tile Map
    Attached Tile Data to the respective Tile

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+
+    [SerializeField]
+    Tilemap map;
+    [SerializeField]
+    List<TileData> tileDatas; // gets all the tileData's
+
+    Dictionary<TileBase, TileData> dataFromTiles;
+
+
+    void Awake()
+    {
+        //gets all the tiles (and their data) in a dictionary
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+
+        foreach(TileData tileData in tileDatas)
+        {
+            foreach(Tile tile in tileData.tiles)
+            {
+                dataFromTiles.Add(tile, tileData);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //testing code
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            TileBase tile = map.GetTile(gridPosition);
+
+            int move = dataFromTiles[tile].movementCost;
+            
+            print(tile + ", " + move);
+
+
+
+
+        }
+    }
+}
